18886f6b7ed3994e9aaba55419a77e19eb3f249a
shahbaazbuksh <48543913+shahbaazbuksh@users.noreply.github.com>
2023-10-17 09:02:27 +0530

    Request / Response

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log("Shahbaaz");
+  const url = req.url;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>My First Page</title></head>");
+    res.write("<body><h1>Welcome Home</h1></body>");
+    res.write("</html>");
+    res.end();
+  } else if (url === "/about") {
+    res.write("<html>");
+    res.write("<head><title>My First Page</title></head>");
+    res.write("<body><h1>Welcome to About Us page</h1></body>");
+    res.write("</html>");
+    res.end();
+  } else if (url === "/node") {
+    res.write("<html>");
+    res.write("<head><title>My First Page</title></head>");
+    res.write("<body><h1>Welcome to my node JS project</h1></body>");
+    res.write("</html>");
+    res.end();
+  }
+  // res.setHeader("Content-Type", "text/html");
+  // res.write("<html>");
+  // res.write("<head><title>My First Page</title></head>");
+  // res.write("<body><h1>Hello from my node JS server</h1></body>");
+  // res.write("</html>");
+  // res.end();
 });
 
-server.listen(4000);
+server.listen(3000);
