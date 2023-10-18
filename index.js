61d5c638cd319e085e50a9728081737682d45b28
shahbaazbuksh <48543913+shahbaazbuksh@users.noreply.github.com>
2023-10-18 14:52:17 +0530

    Clean Up Code

--- index.js
+++ index.js
@@ -1,49 +1,9 @@
 const http = require("http");
-const fs = require("fs");
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
+const routes = require("./route");
 
-  if (url === "/") {
-    res.write("<html>");
-    res.write("<head><title>Enter Message</title></head>");
-    res.write("<body>");
-    fs.readFile("message.txt", "utf8", (err, data) => {
-      if (!err) {
-        res.write(`<p>${data}</p>`);
-      }
-      res.write(
-        "<form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form>"
-      );
-      res.write("</body>");
-      res.write("</html>");
-      return res.end();
-    });
-  } else if (url === "/message" && method === "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      body.push(chunk);
-    });
+console.log(routes.someText);
 
-    req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split("=")[1];
-
-      fs.writeFile("message.txt", message, (err) => {
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        return res.end();
-      });
-    });
-  } else {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>My First Page</title></head>");
-    res.write("<body><h1>Hello from my NodeJS server</h1></body>");
-    res.write("</html>");
-    res.end();
-  }
-});
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
---
+++ route.js
@@ -0,0 +1,56 @@
+const fs = require("fs");
+
+const requestHandler = (req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write("<body>");
+    fs.readFile("message.txt", "utf8", (err, data) => {
+      if (!err) {
+        res.write(`<p>${data}</p>`);
+      }
+      res.write(
+        "<form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form>"
+      );
+      res.write("</body>");
+      res.write("</html>");
+      return res.end();
+    });
+  } else if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+
+      fs.writeFile("message.txt", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
+  } else {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>My First Page</title></head>");
+    res.write("<body><h1>Hello from my NodeJS server</h1></body>");
+    res.write("</html>");
+    res.end();
+  }
+};
+
+// module.exports = {
+//   handler: requestHandler,
+//   someText: "Some hard coded text",
+// };
+
+// module.exports.handler = requestHandler;
+// module.exports.someText = "Some hard coded text";
+
+exports.handler = requestHandler;
+exports.someText = "Some hard coded text";
