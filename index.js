6ef48625a9216b36b6e5c7b1119558fd45ee6ad4
shahbaazbuksh <48543913+shahbaazbuksh@users.noreply.github.com>
2023-11-05 01:10:01 +0530

    Intro to Middlewares

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
-const http = require("http");
-
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the middleware");
-  next();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/add-product", (req, res, next) => {
+  res.send(
+    "<form action='/add-product' method='POST'><input type='text' name='product'><input type='text' name='size'><button type='submit'>Submit</button></form>"
+  );
+});
+
+app.post("/add-product", (req, res, next) => {
+  console.log(req.body.product);
+  console.log(req.body.size);
+  res.redirect("/");
 });
 
 app.use((req, res, next) => {
-  console.log("In another middleaware");
   res.send("<h1>Hello from Express!</h1>");
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
