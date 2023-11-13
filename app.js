9df5876bdc3e8597e49da4d8cae80efb56de5a42
shahbaazbuksh <48543913+shahbaazbuksh@users.noreply.github.com>
2023-11-13 22:04:12 +0530

    Dynamic Routes Part 1

--- app.js
+++ app.js
@@ -3,27 +3,21 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const errorController = require("./controllers/error");
+
 const app = express();
 
 app.set("view engine", "ejs");
-app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
-const contactRoutes = require("./routes/contact");
-const productsController = require("./controllers/products");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
-app.use("/admin", contactRoutes);
 
-// app.use((req, res, next) => {
-//   res.status(404).render("404", { pageTitle: "Page Not Found" });
-// });
-
-app.use(productsController.getError);
+app.use(errorController.get404);
 
 app.listen(3000);
--- models/product.js
+++ models/product.js
@@ -1,5 +1,5 @@
-const path = require("path");
 const fs = require("fs");
+const path = require("path");
 
 const p = path.join(
   path.dirname(require.main.filename),
@@ -11,17 +11,22 @@ const getProductsFromFile = (cb) => {
   fs.readFile(p, (err, fileContent) => {
     if (err) {
       cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
     }
-    cb(JSON.parse(fileContent));
   });
 };
 
 module.exports = class Product {
-  constructor(t) {
-    this.title = t;
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
+    this.id = Math.random().toString();
     getProductsFromFile((products) => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
@@ -32,5 +37,12 @@ module.exports = class Product {
 
   static fetchAll(cb) {
     getProductsFromFile(cb);
+  }
+
+  static findById(id, cb) {
+    getProductsFromFile((products) => {
+      const product = products.find((p) => p.id === id);
+      cb(product);
+    });
   }
 };
