2ae2a803fe4c9a87c84b65cd5ed39909b240f874
shahbaazbuksh <48543913+shahbaazbuksh@users.noreply.github.com>
2024-01-03 21:50:21 +0530

    Full stack Booking Appointment App

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const ejs = require('ejs');
-const sequelize = require('./database');
 
-const appointmentController = require('./controllers/appointmentController');
+const errorController = require('./controllers/error');
+const sequelize = require('./util/database');
 
 const app = express();
 
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.set('views', 'views');
 
-app.use('/', appointmentController);
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-sequelize.sync().then(() => {
-    app.listen(3000, () => {
-        console.log('Server is running on port 3000');
-    });
-});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+sequelize
+  .sync()
+  .then(result => {
+    // console.log(result);
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
